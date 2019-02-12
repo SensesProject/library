e885ce2182fb3959ee658835b569a4c68ea6f3ae
Fidel Thomet <fidelthomet@Fidels-MacBook-Pro.local>
2019-02-12 17:14:17 +0100

    [Update] removed boilerplate / basic routing setup

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Style from './views/Style.vue'
+import Components from './views/Components.vue'
 
 Vue.use(Router)
 
@@ -8,16 +9,17 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'style',
+      component: Style
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/components',
+      name: 'components',
+      component: Components
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      // component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
