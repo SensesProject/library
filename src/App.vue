6b5a9e427e89b7df91f0e314461e31757f7a7fde
Fidel Thomet <me@fidelthomet.com>
2019-02-12 17:29:40 +0100

    [Style] reset boilerplate style

--- src/App.vue
+++ src/App.vue
@@ -10,21 +10,18 @@
 </template>
 
 <style lang="scss">
+@import "/style/base";
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: 'IBM Plex Sans', sans-serif;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+</style>
+<style scoped lang="scss">
+@import "/style/variables";
+#app {
+  nav {
+    width: 100vw;
+    height: 4rem;
+    border-bottom: 1px solid $color-pale-gray;
   }
 }
 </style>
