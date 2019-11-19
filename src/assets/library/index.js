01d1c31fec26161dec072c1222f6263d7ab6e484
Fidel Thomet <fidelthomet@Fidels-MacBook-Air.local>
2019-11-19 11:22:52 +0100

    [Update] 📜 added IntersectionObserverFractured to track scrolling

--- src/assets/library/index.js
+++ src/assets/library/index.js
@@ -1,6 +1,7 @@
 import SensesLogo from '@/components/SensesLogo.vue'
 import LayoutScrollytelling from '@/components/LayoutScrollytelling.vue'
 import IntersectionObserver from '@/components/IntersectionObserver.vue'
+import IntersectionObserverFracture from '@/components/IntersectionObserverFracture.vue'
 import SensesMenu from '@/components/SensesMenu.vue'
 import SensesFalafel from '@/components/SensesFalafel.vue'
 import SensesRadio from '@/components/SensesRadio.vue'
@@ -11,6 +12,7 @@ export default [
   SensesLogo,
   LayoutScrollytelling,
   IntersectionObserver,
+  IntersectionObserverFracture,
   SensesMenu,
   SensesFalafel,
   SensesRadio,
