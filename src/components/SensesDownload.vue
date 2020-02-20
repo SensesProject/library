a10aa18f79ab70cba384a3b032712135abbd5679
z3to <6gqrbj@posteo.org>
2020-02-20 13:42:57 +0100

    Functionality added

--- src/components/SensesDownload.vue
+++ src/components/SensesDownload.vue
@@ -1,40 +1,381 @@
 <template>
   <modal
-    :scrollable="scrollable"
+    :scrollable="true"
+    :width="800"
+    @closed="closed"
     name="download"
     height="auto"
-    :width="width"
   >
-    <h1>Senses Download {{ test }}</h1>
+    <div class="page-download">
+      <header class="download-header">
+        <h1 class="mono">
+          {{ title }}
+        </h1>
+        <span v-if="downloadElements.length" class="caption">Available downloads</span>
+        <ul v-if="downloadElements.length" class="list">
+          <li
+            v-for="element in downloadElements"
+            :class="{ isActive: element.isActive, clickable: !element.isActive }"
+            @click="() => setActive(element.id)"
+          >
+            {{ element.label }}
+          </li>
+        </ul>
+        <span v-else>No downloads available for this module</span>
+      </header>
+      <div v-if="activeID && item" class="download-content">
+        <div class="download-download">
+          <h2>{{ item.label }}</h2>
+          <div>
+            <a :href="item.link" class="btn btn--action">Download</a>
+          </div>
+          <p class="description">
+            {{ item.description }}
+          </p>
+        </div>
+        <dl class="download-details">
+          <dt class="caption">
+            Preview
+          </dt>
+          <dd class="gallery">
+            <ul class="previews">
+              <li v-for="element in item.previews" class="preview">
+                <img :src="`/previews/${element}`">
+              </li>
+            </ul>
+          </dd>
+          <dt class="caption">
+            Authors
+          </dt>
+          <dd>{{ author }}</dd>
+          <dt v-if="reference" class="caption">
+            Reference
+          </dt>
+          <dd v-if="reference">
+            <SensesCopy :content="item.reference" />
+          </dd>
+          <dt v-if="reference" class="caption">
+            URL
+          </dt>
+          <dd v-if="reference">
+            <SensesCopy content="https://climatescenarios.org/#primerdownloads" />
+          </dd>
+          <dt class="caption">
+            Licence
+          </dt>
+          <dd>{{ item.licence }}</dd>
+        </dl>
+        <footer class="download-footer">
+          <strong>You are free to</strong>
+          <ul>
+            <li>Copy and redistribute the material in any medium or format</li>
+            <li>Remix, transform, and build upon the material for any purpose, even commercially</li>
+          </ul>
+          <strong>Under the following terms</strong>
+          <ul>
+            <li>You must give appropriate credit, provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.</li>
+            <li>If you remix, transform, or build upon the material, you must distribute your contributions under the same license as the original.</li>
+            <li>You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits.</li>
+          </ul>
+        </footer>
+        <span @click="closed" class="close">&times;</span>
+      </div>
+      <div v-else class="download-error">
+        <h2>Download can not be found</h2>
+      </div>
+    </div>
   </modal>
 </template>
 
 <script>
+import { get, map, includes, head, find } from 'lodash'
+import axios from 'axios'
+import SensesCopy from '~/components/SensesCopy.vue'
+
+// Function to make sentence from author array
+const chain = (a) => { return [a.slice(0, -1).join(', '), a.slice(-1)[0]].join(a.length < 2 ? '' : ' and ') }
+
 export default {
   name: 'SensesDownload',
+  components: {
+    SensesCopy
+  },
   props: {
-    scrollable: {
-      type: Boolean,
-      default: true,
-      docs: 'Is the modal scrollable'
+    selected: { // The id of the currently selected download. This will be checked again
+      type: [Number, Boolean],
+      default: false,
+      docs: 'The currently selected download id'
     },
-    width: {
-      type: Number,
-      default: 800,
-      docs: 'Width of the modal in px'
-    },
-    test: {
+    title: {
       type: String,
-      default: 'Foo'
+      default: 'Untitled module',
+      docs: 'Title of the module'
+    },
+    close: {
+      type: Function,
+      default: () => {},
+      docs: 'Function that is called once the window closes'
+    },
+    ids: {
+      type: Array,
+      default: () => {
+        return []
+      },
+      docs: 'List of ids of downloads for this module'
+    }
+  },
+  data () {
+    return {
+      downloads: [], // A list of downloads fetched from external JSON file
+      activeID: false
+    }
+  },
+  computed: {
+    availableIDS () {
+      // Get a list of available download ids
+      return map(this.downloads, 'id')
+    },
+    item () {
+      // Find the download item in the list of downloads from the id
+      return find(this.downloads, { id: this.activeID })
+    },
+    author () {
+      // Extract the authors for this download
+      return chain(get(this.item, 'authors', []))
+    },
+    reference () {
+      // Extract the reference for this download
+      return get(this.item, 'reference')
+    },
+    downloadElements () {
+      // Create a list of download items for this module
+      const { ids, activeID, downloads } = this
+      if (ids && ids.length) {
+        return map(ids, (id) => {
+          const item = find(downloads, { id })
+          const label = get(item, 'label', 'unnamed item')
+          const isActive = id === activeID
+          return {
+            id,
+            ...item,
+            label,
+            isActive
+          }
+        })
+      }
+      return []
+    }
+  },
+  watch: {
+    selected (v) {
+      if (v) {
+        this.setActive(v)
+        this.$modal.show('download')
+      }
+    }
+  },
+  mounted () {
+    // Load the list of downloads
+    this.loadDownloads()
+  },
+  methods: {
+    closed () {
+      // Triggered when the close button is clicked or the outer area
+      this.$modal.hide('download')
+      this.activeID = false
+      // Exceute the function passed to the component
+      this.close()
+    },
+    setActive (v) {
+      if (v) {
+        if (includes(this.availableIDS, v)) {
+          // The id is present in the list of downloads
+          this.activeID = v
+        } else if (this.ids.length) {
+          // The module has a list of downloads
+          const first = head(this.ids) // Get the first in the list
+          if (includes(this.availableIDS, first)) { // Check again if id present
+            this.activeID = first // Select the first one
+          } else {
+            this.activeID = false // Set active element to false
+          }
+        } else {
+          this.activeID = false // Set active element to false
+        }
+      } else {
+        // Set the active element to false
+        this.activeID = false
+      }
+    },
+    loadDownloads () {
+      axios.get('https://dev.climatescenarios.org/settings/downloads.json')
+        .then((response) => {
+          this.downloads = response.data.downloads
+        })
     }
   }
 }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
 @import "../style/global.scss";
-</style>
 
-<style lang="scss">
-@import "../style/global.scss";
+.page-download {
+  // margin: $spacing;
+  display: grid;
+  grid-row-gap: $spacing;
+  position: relative;
+  z-index: 10;
+
+  .download-download, .download-details {
+    margin: $spacing;
+    margin-bottom: 0;
+    margin-top: 0;
+  }
+
+  .download-download {
+    display: grid;
+    align-items: center;
+    grid-template-columns: 2fr 1fr;
+    grid-column-gap: $spacing;
+    grid-row-gap: $spacing;
+
+    .description {
+      grid-column-end: span 2;
+    }
+  }
+
+  .download-header {
+    // background-color: $color-neon;
+    padding: $spacing;
+    // color: #fff;
+    border-bottom: 1px solid getColor(gray, 80);
+
+    h1 {
+      font-size: 2.3rem;
+      font-style: italic;
+      line-height: 1;
+      letter-spacing: -0.02em;
+      margin-bottom: $spacing / 2;
+    }
+
+    .caption {
+      color: rgba(#000, 0.7);
+    }
+
+    .list {
+      margin-top: $spacing / 4;
+      list-style: none;
+
+      li {
+        padding: 0.2em 1em 0.2em 1.6em;
+        list-style-position: inside;
+        text-indent: -1.6em;
+        line-height: 1.5em;
+        color: $color-neon;
+
+        &:hover, &:hover {
+          color: getColor(neon, 40);
+        }
+
+        &.isActive {
+          color: #000;
+        }
+
+        &:before {
+          content: "— ";
+          color: getColor(gray, 80);
+          margin-right: 0.3em;
+        }
+      }
+    }
+  }
+
+  .download-footer {
+    border-top: 1px solid getColor(gray, 80);
+    padding: $spacing;
+
+    ul {
+      font-size: 0.8rem;
+    }
+
+    strong {
+      font-size: 0.9rem;
+    }
+
+    ul {
+      margin-left: $spacing;
+      margin-bottom: $spacing / 2;
+    }
+
+    .btn {
+      margin-top: $spacing;
+    }
+
+    dl {
+      margin-bottom: $spacing / 2;
+    }
+  }
+
+  .close {
+    position: absolute;
+    top: 0;
+    right: 0;
+    font-size: 1.5rem;
+    display: inline-block;
+    color: $color-neon;
+    cursor: pointer;
+    speak: none;
+    font-variant: normal;
+    text-transform: none;
+    line-height: 1em;
+    margin-top: $spacing / 3;
+    margin-right: $spacing / 2;
+
+    &:hover {
+      color: #000;
+    }
+  }
+
+  .download-error {
+    @include center();
+    margin-bottom: $spacing;
+  }
+
+  .download-details {
+    dd {
+      margin-bottom: $spacing / 2;
+
+      &:last-of-type {
+        margin-bottom: 0;
+      }
+    }
+  }
+
+  .gallery {
+    position: relative;
+    height: 200px;
+  }
+
+  .previews {
+    width: 100%;
+    overflow-y: scroll;
+    display: flex;
+    flex-direction: row;
+    list-style: none;
+    position: absolute;
+
+    .preview {
+      height: 200px;
+      padding: $spacing / 4;
+
+      img {
+        height: 100%;
+        width: auto;
+        box-shadow: $box-shadow--default;
+      }
+    }
+  }
+}
+
 </style>
