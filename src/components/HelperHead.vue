6a393690503380047f6fd4967f301d0a17115491
Fidel Thomet <me@fidelthomet.com>
2020-05-26 15:56:57 +0200

    [Update] HelperHead og/cards images

--- src/components/HelperHead.vue
+++ src/components/HelperHead.vue
@@ -18,14 +18,14 @@ export default {
       type: String,
       default: 'website'
     },
-    card: {
+    img: {
       type: String,
-      default: 'summary'
+      default: 'og-toolkit.jpg'
     }
   },
   computed: {
     head () {
-      const { title, description, type, card } = this
+      const { title, description, type, img } = this
       return `
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -33,17 +33,21 @@ export default {
 <title>${title}</title>
 <meta name="description" content="${description}">
 
-<link rel="icon" sizes="192x192" href="https://dev.climatescenarios.org/favicon-192.png">
-<link rel="apple-touch-icon" href="https://dev.climatescenarios.org/favicon-192.png">
-<link rel="mask-icon" href="https://dev.climatescenarios.org/favicon-svg.png" color="#5263ff">
+<link rel="icon" sizes="192x192" href="https://climatescenarios.org/favicon-192.png">
+<link rel="apple-touch-icon" href="https://climatescenarios.org/favicon-192.png">
+<link rel="mask-icon" href="https://climatescenarios.org/favicon-svg.png" color="#5263ff">
 
 <meta property="og:type" content="${type}">
 <meta property="og:title" content="${title}">
 <meta property="og:description" content="${description}">
 
-<meta name="twitter:card" content="${card}">
+<meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:title" content="${title}">
 <meta name="twitter:description" content="${description}">
+
+<meta name="og:image" content="https://climatescenarios.org/share/${img}">
+<meta name="og:image:width" content="1200">
+<meta name="og:image:height" content="630">
 `.trim()
     }
   }
